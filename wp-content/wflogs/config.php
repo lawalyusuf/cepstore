2edae92a3cd5f2614ac8525c149b328f0686dd11
yusuf <bwitlawalyusuf@gmail.com>
2025-11-04 15:56:48 +0100

    Cepstore Demo SIte

---
+++ wp-content/wflogs/config.php
@@ -0,0 +1,6 @@
+<?php exit('Access denied'); __halt_compiler(); ?>
+******************************************************************
+This file is used by the Wordfence Web Application Firewall. Read 
+more at https://docs.wordfence.com/en/Web_Application_Firewall_FAQ
+******************************************************************
+a:7:{s:9:"wafStatus";s:13:"learning-mode";s:30:"learningModeGracePeriodEnabled";i:1;s:23:"learningModeGracePeriod";i:1762871388;s:7:"authKey";s:64:"Z|BrFS8KW0/0#^S[z-RESs3iTtJOaZZQx>&Y#%{1u!@Scc#%7U*/2enbPdYMdE5_";s:7:"version";s:5:"1.1.0";s:11:"wafDisabled";b:0;s:13:"attackDataKey";i:630;}
